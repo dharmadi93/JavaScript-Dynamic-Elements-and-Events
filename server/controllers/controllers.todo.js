9a10809b869ce451c370be141947f8b465373ae2
Dharmadi Tanamas <dharmadi93@gmail.com>
2016-11-13 14:43:09 +0700

    create controller skeleton for todo

---
+++ server/controllers/controllers.todo.js
@@ -0,0 +1,27 @@
+const Todo = require('../models/models.todo')
+
+module.exports = {
+    getTodos: (req, res) => {
+
+    },
+
+    getTodoById: (req, res) => {
+
+    }
+
+    createDoto: (req, res) => {
+
+    },
+
+    deleteTodo: (req, res) => {
+
+    },
+
+    updateTodo: (req, res) => {
+
+    },
+
+    updateTodoStatus: (req, res) => {
+
+    }
+}

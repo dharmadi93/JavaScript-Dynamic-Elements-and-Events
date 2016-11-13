07acfc140a792822196b0f3444b7615a87f541fc
Dharmadi Tanamas <dharmadi93@gmail.com>
2016-11-13 14:31:50 +0700

    require all onfic

--- server/app.js
+++ server/app.js
@@ -1,12 +1,18 @@
+require('dotenv').config()
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+const cors = require('cors')
+
+const mongoose = require('mongoose')
+mongoose.connect(proccess.env.DATABASE)
+mongoose.Promise = global.promise
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -22,8 +28,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/api/todo', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
